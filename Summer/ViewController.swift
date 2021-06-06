cbebcf4206d6d2674f9293c46db58cab37ebd6c2
donghyun <hbull@hanmail.net>
2021-06-06 23:39:19 +0900

    디자인 수정

--- Summer/ViewController.swift
+++ Summer/ViewController.swift
@@ -8,18 +8,18 @@
 import UIKit
 
 extension UIImage {
-    func createFilteredImage(filterName: String, image: UIImage) -> UIImage {
+    func createFilteredImage(filterName: String, intensity: Double = 1) -> UIImage {
         let context = CIContext(options: nil)
-        let source = CIImage(image: image)
+        let source = CIImage(image: self)
 
         let filter = CIFilter(name: filterName)
         
         filter?.setDefaults()
         filter?.setValue(source, forKey: kCIInputImageKey)
-
+//        filter?.setValue(intensity, forKey: kCIInputIntensityKey)
+        
         let outputCGImage = context.createCGImage((filter?.outputImage!)!, from: (filter?.outputImage!.extent)!)
 
-        // 5 - convert filtered CGImage to UIImage
         let filteredImage = UIImage(cgImage: outputCGImage!)
 
         return filteredImage
@@ -28,28 +28,37 @@ extension UIImage {
 
 class FilterItemCell: UICollectionViewCell {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
 }
 
+struct Filter {
+    let name: String
+    let identifier: String
+}
 
 final class FilterManager {
-    var filters = [
-        "No Filter",
-        "CIPhotoEffectChrome",
-        "CIPhotoEffectFade",
-        "CIPhotoEffectInstant",
-        "CIPhotoEffectMono",
-        "CIPhotoEffectNoir",
-        "CIPhotoEffectProcess",
-        "CIPhotoEffectTonal",
-        "CIPhotoEffectTransfer",
-        "CILinearToSRGBToneCurve",
-        "CISRGBToneCurveToLinear"
+    let filters = [
+        Filter(name: "Vivid", identifier: "CIPhotoEffectChrome"),
+        Filter(name: "Fade", identifier: "CIPhotoEffectFade"),
+        Filter(name: "Instant", identifier: "CIPhotoEffectInstant"),
+        Filter(name: "Mono", identifier: "CIPhotoEffectMono"),
+        Filter(name: "Noir", identifier: "CIPhotoEffectNoir"),
+        Filter(name: "Process", identifier: "CIPhotoEffectProcess"),
+        Filter(name: "Tonal", identifier: "CIPhotoEffectTonal"),
+        Filter(name: "Transfer", identifier: "CIPhotoEffectTransfer"),
+        Filter(name: "Curve", identifier: "CILinearToSRGBToneCurve"),
+        Filter(name: "Linear", identifier: "CISRGBToneCurveToLinear")
     ]
 }
 
 class ViewController: UIViewController {
     private let filterManager = FilterManager()
-    private var originalImage: UIImage?
+    private var originalImage: UIImage = UIImage(named: "Sample")!
+    private var selectedIndex: Int?
+    
+    private lazy var thumbnails: [UIImage] = {
+        filterManager.filters.map{UIImage(named: "Thumbnail")!.createFilteredImage(filterName: $0.identifier)}
+    }()
     
     @IBOutlet weak private var photoView: UIImageView!
     @IBOutlet weak private var collectionView: UICollectionView!
@@ -57,7 +66,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.photoView.image = originalImage
+        self.photoView.layer.cornerRadius = 4
+        self.photoView.layer.masksToBounds = true
+        self.photoView.layer.cornerCurve = .continuous
     }
     
     @IBAction func importPhoto() {
@@ -67,6 +79,13 @@ class ViewController: UIViewController {
         
         self.present(imagePickerController, animated: true, completion: nil)
     }
+    
+    @IBAction func save() {
+        let controller = UIAlertController(title: "저장되었습니다", message: nil, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        controller.addAction(okAction)
+        present(controller, animated: true, completion: nil)
+    }
 }
 
 extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
@@ -74,6 +93,8 @@ extension ViewController: UIImagePickerControllerDelegate & UINavigationControll
         if let photo = info[.originalImage] as? UIImage {
             self.photoView.image = photo
             self.originalImage = photo
+            self.selectedIndex = nil
+            self.collectionView.reloadData()
         }
         
         picker.dismiss(animated: true, completion: nil)
@@ -91,28 +112,26 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FilterItemCell", for: indexPath) as! FilterItemCell
-        let sample = UIImage(named: "Sample")!
         
-        var image = sample
-        
-        if indexPath.row > 0 {
-            image = sample.createFilteredImage(filterName: filterManager.filters[indexPath.row], image: sample)
-        }
-        
-        cell.imageView.image = image
+        cell.imageView.image = thumbnails[indexPath.row]
+        cell.titleLabel.textColor = indexPath.item == selectedIndex ? .black : .lightGray
+        cell.titleLabel.text = filterManager.filters[indexPath.item].name
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let filter = filterManager.filters[indexPath.item]
-        if let image = originalImage {
-            let filteredImage = image.createFilteredImage(filterName: filter, image: image)
-            self.photoView.image = filteredImage
-        }
+        
+        let filteredImage = originalImage.createFilteredImage(filterName: filter.identifier)
+        
+        self.photoView.image = filteredImage
+        
+        self.selectedIndex = indexPath.item
+        self.collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 100, height: collectionView.frame.height)
+        return CGSize(width: 70, height: 100)
     }
 }
