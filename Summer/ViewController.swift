b2d156897a2aceb5ff3f6ffde3b40ab9a59eaa7d
donghyun <hbull@hanmail.net>
2021-07-04 16:07:20 +0900

    최종 버전

--- Summer/ViewController.swift
+++ Summer/ViewController.swift
@@ -7,16 +7,55 @@
 
 import UIKit
 
-extension UIImage {
-    func createFilteredImage(filterName: String, intensity: Double = 1) -> UIImage {
-        let context = CIContext(options: nil)
-        let source = CIImage(image: self)
+protocol ImageFilter {
+    var name: String { get }
+    
+    func filter(image: UIImage) -> UIImage
+}
 
-        let filter = CIFilter(name: filterName)
+extension UIImage {
+    func resize(to targetSize: CGSize) -> UIImage? {
+        let size = self.size
+        
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        let rect = CGRect(origin: .zero, size: newSize)
+        
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        self.draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return newImage
+    }
+}
+
+class FilterItemCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+}
+
+struct Filter: ImageFilter {
+    let name: String
+    let identifier: String
+    
+    func filter(image: UIImage) -> UIImage {
+        let context = CIContext(options: nil)
+        let source = CIImage(image: image)
+
+        let filter = CIFilter(name: identifier)
         
         filter?.setDefaults()
         filter?.setValue(source, forKey: kCIInputImageKey)
-//        filter?.setValue(intensity, forKey: kCIInputIntensityKey)
         
         let outputCGImage = context.createCGImage((filter?.outputImage!)!, from: (filter?.outputImage!.extent)!)
 
@@ -26,18 +65,8 @@ extension UIImage {
     }
 }
 
-class FilterItemCell: UICollectionViewCell {
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var titleLabel: UILabel!
-}
-
-struct Filter {
-    let name: String
-    let identifier: String
-}
-
 final class FilterManager {
-    let filters = [
+    let filters: [ImageFilter] = [
         Filter(name: "Vivid", identifier: "CIPhotoEffectChrome"),
         Filter(name: "Fade", identifier: "CIPhotoEffectFade"),
         Filter(name: "Instant", identifier: "CIPhotoEffectInstant"),
@@ -57,22 +86,40 @@ class ViewController: UIViewController {
     private var selectedIndex: Int?
     
     private lazy var thumbnails: [UIImage] = {
-        filterManager.filters.map{UIImage(named: "Thumbnail")!.createFilteredImage(filterName: $0.identifier)}
+        filterManager.filters.map{$0.filter(image: UIImage(named: "Thumbnail")!)}
     }()
     
     @IBOutlet weak private var photoView: UIImageView!
     @IBOutlet weak private var collectionView: UICollectionView!
-    @IBOutlet weak private var slider: UISlider!
+    
+    @IBOutlet weak private var photoViewLeading: NSLayoutConstraint!
+    @IBOutlet weak private var photoViewTrailing: NSLayoutConstraint!
+    
+    @IBOutlet weak private var toolBar: UIToolbar!
+    
+    @IBOutlet weak private var maskView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.maskView.isHidden = true
+        
         self.photoView.image = originalImage
         self.photoView.layer.cornerRadius = 4
         self.photoView.layer.masksToBounds = true
         self.photoView.layer.cornerCurve = .continuous
+        
+        self.toolBar.setShadowImage(UIImage(), forToolbarPosition: .bottom)
+        
+        self.toolBar.layer.shadowColor = UIColor.black.cgColor
+        self.toolBar.layer.shadowRadius = 5
+        self.toolBar.layer.shadowOpacity = 0.05
+        self.toolBar.layer.shadowOffset = CGSize(width: 0, height: -10)
     }
     
     @IBAction func importPhoto() {
+
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else { return }
         let imagePickerController = UIImagePickerController()
         
         imagePickerController.delegate = self
@@ -80,28 +127,58 @@ class ViewController: UIViewController {
         self.present(imagePickerController, animated: true, completion: nil)
     }
     
+    @IBAction func presentCamera() {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else { return }
+        let imagePickerController = UIImagePickerController()
+        
+        imagePickerController.sourceType = .camera
+        imagePickerController.allowsEditing = true // 촬영 후 편집할 수 있는 부분이 나온다.
+        imagePickerController.delegate = self
+        
+        present(imagePickerController, animated: true, completion: nil)
+    }
+    
     @IBAction func save() {
+        guard let index = selectedIndex else { return }
+        
         let controller = UIAlertController(title: "저장되었습니다", message: nil, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
         controller.addAction(okAction)
         present(controller, animated: true, completion: nil)
+        
+        let filter = filterManager.filters[index]
+        let image = filter.filter(image: originalImage)
+        UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
     }
 }
 
 extension ViewController: UIImagePickerControllerDelegate & UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let photo = info[.originalImage] as? UIImage {
-            self.photoView.image = photo
-            self.originalImage = photo
-            self.selectedIndex = nil
-            self.collectionView.reloadData()
+            maskView.isHidden = false
+            
+            DispatchQueue.global(qos: .background).async {
+                let resizedImage = photo.resize(to: CGSize(width: photo.size.width / 4, height: photo.size.height / 4))!
+                
+                DispatchQueue.main.async { [weak self] in
+                    let horizontalSpacing: CGFloat = photo.size.width > photo.size.height ? 0 : 15
+                    
+                    self?.photoView.image = resizedImage
+                    self?.selectedIndex = nil
+                    self?.collectionView.reloadData()
+                    self?.photoViewLeading.constant = horizontalSpacing
+                    self?.photoViewTrailing.constant = horizontalSpacing
+                    
+                    self?.maskView.isHidden = true
+                }
+            }
         }
         
         picker.dismiss(animated: true, completion: nil)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        
+        picker.dismiss(animated: true, completion: nil)
     }
 }
 
@@ -123,7 +200,7 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let filter = filterManager.filters[indexPath.item]
         
-        let filteredImage = originalImage.createFilteredImage(filterName: filter.identifier)
+        let filteredImage = filter.filter(image: originalImage)
         
         self.photoView.image = filteredImage
         
